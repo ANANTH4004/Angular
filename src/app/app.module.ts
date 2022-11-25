eb9e0b48e6e44d671c32bc08a0f57732b9c93b19
ANANTH4004 <balamuruganananth@gmail.com>
2022-11-25 19:06:34 +0530

    ng-container ngFor ngIf in structural component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { FComponentComponent } from './f-component/f-component.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
+import { StructuralComponent } from './structural/structural.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FComponentComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
+    StructuralComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/structural/structural.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structural',
+  templateUrl: './structural.component.html',
+  styleUrls: ['./structural.component.css']
+})
+export class StructuralComponent {
+
+  people : any[];
+ constructor(){
+ this.people =[{
+  'age':21,
+  'name':"Anand"
+ },{
+  'age':22,
+  'name':"Nithya"
+ },{
+  'age':23,
+  'name':"Varun"
+ },{
+  'age':34,
+  'name':"Samantha"
+ },{
+  'age':35,
+  'name':"Kathija"
+ },{
+  'age':36,
+  'name':"Bidhu"
+ }]
+ }
+}
